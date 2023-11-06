ef5337195dbf2143f4119560c84339ab7a4d6d67
PrakatNeupane <neupaneprakat@gmail.com>
2023-11-06 13:19:05 +1100

    server initials

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const port = 3001;
+
+//handle api security
+app.use(helmet());
+
+// handle cors error
+app.use(cors());
+
+// mongoDB connection
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL);
+
+if (process.env.NODE_ENV !== "production") {
+  const mDb = mongoose.connection;
+  mDb.on("open", () => {
+    console.log("MongoDB is now connected");
+  });
+
+  mDb.on("error", (error) => {
+    console.log(error);
+  });
+  // logger
+  app.use(morgan("tiny"));
+}
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// load routers
+const userRouter = require("./src/routers/user.router");
+const ticketRouter = require("./src/routers/ticket.router");
+
+// Use routers
+app.use("/v1/user", userRouter);
+app.use("/v1/ticket", ticketRouter);
+
+//Error handler
+const handleError = require("./src/utils/errorHandler");
+app.use((req, res, next) => {
+  const error = new Error("Resources not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  handleError(error, res);
+});
+
+app.listen(port, () => {
+  console.log(`listening on port http://localhost:${port}`);
+});
---
+++ src/routers/user.router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const { insertUser } = require("../model/user/User.model");
+
+router.all("/", (req, res, next) => {
+  next();
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const result = await insertUser(req.body);
+    console.log(result);
+    res.json({
+      message: "New user created",
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: error.message });
+  }
+});
+
+module.exports = router;
